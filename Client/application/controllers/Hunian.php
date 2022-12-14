3c85ac9f2346cfb18f9caa645c2b94638a5b37ee
teguhh18 <teguhbudiono147@gmail.com>
2022-12-14 23:10:37 +0700

    master: buat controller(Hunian) untuk vw_hunian

---
+++ Client/application/controllers/Hunian.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Hunian extends CI_Controller {
+   
+    public function index()
+	{
+		$data["tampil"] = json_decode($this->client->simple_get(APIHUNIAN));
+		$this->load->view('vw_hunian', $data);
+
+		// foreach($data["tampil"] -> mahasiswa as $result){
+		// 	echo $result->npm_mhs."<br>";
+		// 	echo $result->nama_mhs."<br>";
+		// }
+
+		//$this->load->load->view('vw_mahasiswa',$data);
+	}
+}
--- Client/application/views/vw_hunian.php
+++ Client/application/views/vw_hunian.php
@@ -53,25 +53,38 @@
             </div>
         </header>
         <!-- Section-->
+        <?php 
+            
+            $no = 1;
+            foreach (   $tampil ->  hunian as $result    ) 
+            {
+    
+            ?>
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." /> -->
+                            <div class="card-img-top">
+                            <?= $result->gambar; ?>
+                            </div>
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
                                     <!-- Product name-->
-                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <h5 class="fw-bolder"><?= $result->nama_hunian;     ?></h5>
                                     <!-- Product price-->
-                                    $40.00 - $80.00
+                                    <div>
+                                    <?= $result->harga_hunian;     ?>
+                                    </div>
+                                    <!-- $40.00 - $80.00 -->
                                 </div>
                             </div>
                             <!-- Product actions-->
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Sewa</a></div>
                             </div>
                         </div>
                     </div>
@@ -255,6 +268,10 @@
                 </div>
             </div>
         </section>
+        <?php
+            $no++; 
+            }
+        ?> 
         <!-- Footer-->
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
