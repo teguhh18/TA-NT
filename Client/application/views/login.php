f7b34d3b45f092413d7fd9f3556a6e5698a70c78
Muhan212223 <muhan212223@gmail.com>
2023-01-07 22:50:50 +0700

    abdian: form-login

--- Client/application/views/login.php
+++ Client/application/views/login.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Kost Tegar</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?= base_url('assets/assets_stisla'); ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url('assets/assets_stisla'); ?>/assets/css/components.css">
+</head>
+
 <body>
   <div id="app">
     <section class="section">
@@ -5,13 +21,13 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="<?= base_url('Client/assets/login/head.jpg'); ?>" alt="logo" width="300" class="shadow-light rounded-circle">
+              <img src="<?= base_url('assets/login/head.jpg'); ?>" alt="logo" width="300" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
               <div class="card-header"><h4>Login</h4></div>
 
-              <span class="m-2"><?= $this->session->flashdata('pesan'); ?></span>
+              <!-- <span class="m-2"><?= $this->session->flashdata('pesan'); ?></span> -->
 
               <div class="card-body">
                 <form method="POST" action="<?= base_url('auth/login'); ?>">
