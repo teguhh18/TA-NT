72e4a4750516f4423ca00e9a91c2d384a7fe6709
teguhh18 <teguhbudiono147@gmail.com>
2023-01-27 02:21:23 +0700

    master: menyesuaikan href pada navigasi

--- Client/application/controllers/Hunian.php
+++ Client/application/controllers/Hunian.php
@@ -5,15 +5,13 @@ class Hunian extends CI_Controller {
    
     public function index()
 	{
+		$this->load->view('dashboard');
+	}
+
+	public function data_hunian()
+	{
 		$data['tampil'] = json_decode($this->client->simple_get(APIHUNIAN));
 		$this->load->view('vw_hunian', $data);
-
-		// foreach($data["tampil"] -> mahasiswa as $result){
-		// 	echo $result->npm_mhs."<br>";
-		// 	echo $result->nama_mhs."<br>";
-		// }
-
-		//$this->load->load->view('vw_mahasiswa',$data);
 	}
 
 	public function detailHunian()
--- Client/application/views/dashboard.php
+++ Client/application/views/dashboard.php
@@ -22,9 +22,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?= base_url('Hunian') ?>">Home</a>
                         </li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Kamar Kost</a>
+                        <li class="nav-item"><a class="nav-link" href="<?= base_url('Hunian/data_hunian') ?>">Kamar Kost</a>
                         </li>
 
                         <li class="nav-item"><a class="nav-link" href="#!">Penyewaan</a>
