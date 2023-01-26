eda4f396f38a9bfc82832c153124cc72aa393f26
mrasyid <rasyid.787898@gmail.com>
2023-01-27 00:02:56 +0700

    mrasyid: dashboard

---
+++ Client/application/views/dashboard.php
@@ -0,0 +1,100 @@
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <h1>Data Kamar</h1>
+    </div>
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar1.jpg" alt="" width="500" height="350">
+        <div class="item-content">
+          <div class="main-content">
+           
+            <a href="fleet.html"><h4>Kamar Kost Biasa</h4></a>
+            <ul class="post-info">
+              <li>Kamar Kost Harga Murah, Spek Mewah, dijamin Betah. GASS LAHH !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar5.jpg" alt="" width="500" height="350">
+        <div class="item-content">
+          <div class="main-content">
+            
+            <a href="fleet.html"><h4>Kamar Kost VIP</h4></a>
+            <ul class="post-info">
+              <li>Kamar Kost Harga Murah, Spek Mewah, dijamin Betah. GASS LAHH !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar3.jpg" alt="" width="500" height="350">
+        <div class="item-content">
+          <div class="main-content">
+            
+            <a href="fleet.html"><h4>Kamar Kost VIP</h4></a>
+            <ul class="post-info">
+              <li>Kamar Kost Harga Murah, Spek Mewah, dijamin Betah. GASS LAHH !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar4.jpg" alt="" width="500" height="350">
+        <div class="item-content">
+          <div class="main-content">
+            
+            <a href="fleet.html"><h4>Kamar Kost VVIP</h4></a>
+            <ul class="post-info">
+              <li>Kamar Kost Harga Murah, Spek Mewah, dijamin Betah. GASS LAHH !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar5.jpg" alt="" width="500" height="350">
+        <div class="item-content">
+          <div class="main-content">
+            <a href="fleet.html"><h4>Kamar Kost Biasa</h4></a>
+            <ul class="post-info">
+              <li>Kamar Kost Harga Murah, Spek Mewah, dijamin Betah. GASS LAHH !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+
+
+<section class="call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-content">
+          <div class="row">
+            <div class="col-lg-8">
+              <span>Tentang Kost Tegar</span>
+              <p style="color: white;">Kost Tegar merupakan website kosan yang menyediakan banyak kamar yang memiliki fasilitas yang nyaman dan layak. Kost Tegar terletak di Jalan Cengkeh, Gedung Meneng, Rajabasa</p>
+            </div>
+            <div class="col-lg-4">
+              <div class="main-button">
+                <a href="#">Hubungi Kami</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
