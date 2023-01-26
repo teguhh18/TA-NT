9d095e7db1bfe947100afc46f4c050cb37089208
mrasyid <rasyid.787898@gmail.com>
2023-01-27 00:16:24 +0700

    mrasyid: dashboard

--- Client/application/views/dashboard.php
+++ Client/application/views/dashboard.php
@@ -1,11 +1,61 @@
-<div class="main-content">
-  <section class="section">
-    <div class="section-header">
-      <h1>Data Kamar</h1>
-    </div>
-<!-- Page Content -->
-<!-- Banner Starts Here -->
-<div class="main-banner header-text">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Daftar Hunian</title>
+        <!-- Favicon-->
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo base_url("ext/style.css") ?>" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Kost Tegar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Kamar Kost</a>
+                        </li>
+
+                        <li class="nav-item"><a class="nav-link" href="#!">Penyewaan</a>
+                        </li>
+
+                        <li class="nav-item"><a class="nav-link" href="#!">Tentang</a>
+                        </li>
+                        
+                    </ul>
+                    <!-- <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                        f    <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form> -->
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">KOST TEGAR</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Sewa Kamar Dengan Harga Murah</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+
+        <div class="main-banner header-text">
   <div class="container-fluid">
     <div class="owl-banner owl-carousel">
       <div class="item">
@@ -98,3 +148,29 @@
 </section>
 
 
+
+<div style="height: 180px;"></div>
+
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Kost Tegar 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <!-- <script src="ext/scripts.js"></script>
+        <script>
+            // fungsi untuk ke halaman detail hunian
+            // function gotoDetail(nomor_hunian)
+            // {
+            //     location.href='<?php echo site_url("Hunian/detailHunian")?>'+'?nomor_hunian='+nomor_hunian
+            // }
+
+            // fungsi untuk ke halaman detail hunian
+            //function gotoDetail($nomor_hunian)
+            //{
+            //    location.href='<?php echo site_url("Hunian/detailHunian")?>'
+            //}
+        </script> -->
+    </body>
+</html>
