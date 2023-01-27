110b1d50c14593d17db47c7e13c04d8c6c8a12d4
mrasyid <rasyid.787898@gmail.com>
2023-01-27 21:13:24 +0700

    mrasyid: dashboard

--- Client/application/views/dashboard.php
+++ Client/application/views/dashboard.php
@@ -167,26 +167,45 @@
 
 <div style="height: 180px;"></div>
 
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Kost Tegar 2022</p></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <!-- <script src="ext/scripts.js"></script>
-        <script>
-            // fungsi untuk ke halaman detail hunian
-            // function gotoDetail(nomor_hunian)
-            // {
-            //     location.href='<?php echo site_url("Hunian/detailHunian")?>'+'?nomor_hunian='+nomor_hunian
-            // }
-
-            // fungsi untuk ke halaman detail hunian
-            //function gotoDetail($nomor_hunian)
-            //{
-            //    location.href='<?php echo site_url("Hunian/detailHunian")?>'
-            //}
-        </script> -->
+        <!-- end testimonial -->
+      <!--  footer -->
+      <footer id="contact">
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-6">
+                     <div class="titlepage">
+                        <h2>Hubungi Kami</h2>
+                     </div>
+                     <div class="cont">
+                        <h3>Kost Tegar </h3>
+                        <p>Jalan Cengkeh, Gedung Meneng, Rajabasa</p>
+                     </div>
+                  </div>
+                  
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>Copyright 2022 All Right Reserved By <a href=""> Kost Tegar</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
     </body>
 </html>
