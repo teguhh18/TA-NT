adada20d4a85f34fc91cc6dec511f35f494838c3
mrasyid <rasyid.787898@gmail.com>
2023-01-27 21:45:50 +0700

    mrasyid: dashboard

--- Client/application/views/dashboard.php
+++ Client/application/views/dashboard.php
@@ -1,41 +1,4 @@
-
 <!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
-
-    <title>Website Sewa Kost Tegar</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="http://localhost:8080/KOST-TEGAR/assets/assets_shop/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="http://localhost:8080/KOST-TEGAR/assets/assets_shop/css/fontawesome.css">
-    <link rel="stylesheet" href="http://localhost:8080/KOST-TEGAR/assets/assets_shop/css/style.css">
-    <link rel="stylesheet" href="http://localhost:8080/KOST-TEGAR/assets/assets_shop/css/owl.css">
-    <link rel="stylesheet" href="http://localhost:8080/KOST-TEGAR/assets/assets_shop/css/footer.css">
-  </head>
-
-  <body>
-
-    <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-
-    <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
@@ -50,6 +13,22 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="<?php echo base_url("ext/style.css") ?>" rel="stylesheet" />
+        
+        <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/assets_home/css/bootstrap.min.css")?>">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
     </head>
     <body>
         <!-- Navigation-->
@@ -90,12 +69,13 @@
                 </div>
             </div>
         </header>
-<!-- Banner Starts Here -->
-<div class="main-banner header-text">
+        <!-- Section-->
+
+        <div class="main-banner header-text">
   <div class="container-fluid">
     <div class="owl-banner owl-carousel">
       <div class="item">
-        <img src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/images/kamar1.jpg" alt="" width="500" height="350">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar1.jpg" alt="" width="500" height="350">
         <div class="item-content">
           <div class="main-content">
            
@@ -107,7 +87,7 @@
         </div>
       </div>
       <div class="item">
-        <img src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/images/kamar5.jpg" alt="" width="500" height="350">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar5.jpg" alt="" width="500" height="350">
         <div class="item-content">
           <div class="main-content">
             
@@ -119,7 +99,7 @@
         </div>
       </div>
       <div class="item">
-        <img src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/images/kamar3.jpg" alt="" width="500" height="350">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar3.jpg" alt="" width="500" height="350">
         <div class="item-content">
           <div class="main-content">
             
@@ -131,7 +111,7 @@
         </div>
       </div>
       <div class="item">
-        <img src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/images/kamar4.jpg" alt="" width="500" height="350">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar4.jpg" alt="" width="500" height="350">
         <div class="item-content">
           <div class="main-content">
             
@@ -143,7 +123,7 @@
         </div>
       </div>
       <div class="item">
-        <img src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/images/kamar5.jpg" alt="" width="500" height="350">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/kamar5.jpg" alt="" width="500" height="350">
         <div class="item-content">
           <div class="main-content">
             <a href="fleet.html"><h4>Kamar Kost Biasa</h4></a>
@@ -185,51 +165,28 @@
 
 
 
-<div style="height: 180px;"></div>    <footer class="fixed-bottom">
-      <!-- <div class="container"> -->
-        <div class="row">
-          <!-- <div class="col-lg-12">
-            <ul class="social-icons">
-              <li><a href="#">Facebook</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Behance</a></li>
-              <li><a href="#">Linkedin</a></li>
-            </ul>
-          </div> -->
-          <div class="col-md-11">
-            <div class="copyright-text">
-              <p>
-                Copyright © 2022 Kost Tegar
-              </p>
-            </div>
-          </div>
-        </div>
-      <!-- </div> -->
-    </footer>
+<div style="height: 180px;"></div>
 
-    
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Kost Tegar 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <!-- <script src="ext/scripts.js"></script>
+        <script>
+            // fungsi untuk ke halaman detail hunian
+            // function gotoDetail(nomor_hunian)
+            // {
+            //     location.href='<?php echo site_url("Hunian/detailHunian")?>'+'?nomor_hunian='+nomor_hunian
+            // }
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/vendor/jquery/jquery.min.js"></script>
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Additional Scripts -->
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/js/custom.js"></script>
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/js/owl.js"></script>
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/js/slick.js"></script>
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/js/isotope.js"></script>
-    <script src="http://localhost:8080/KOST-TEGAR/assets/assets_shop/js/accordions.js"></script>
-
-    <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
-          t.style.color='#fff';
-          }
-      }
-    </script>
-
-  </body>
+            // fungsi untuk ke halaman detail hunian
+            //function gotoDetail($nomor_hunian)
+            //{
+            //    location.href='<?php echo site_url("Hunian/detailHunian")?>'
+            //}
+        </script> -->
+    </body>
 </html>
