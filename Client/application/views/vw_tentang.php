5a588cf2d1a1b2a1904a850998649a5fd04735bf
Muhan212223 <muhan212223@gmail.com>
2023-01-27 23:38:47 +0700

    abdian: menambah tentang kost

--- Client/application/views/vw_tentang.php
+++ Client/application/views/vw_tentang.php
@@ -7,9 +7,10 @@
         <div class="main-content">
           <div class="row">
             <div class="col-lg-8">
-              <span>Tentang Kost Tegar</span>
-              <p style="color: white;">Kos-kosan merupakan salah satu tempat penyedia jasa penginapan atau tempat tinggal sementara yang memiliki beberapa fasilitas yang tersedia, dan juga mempunyai harga yang telah ditentukan oleh pemilik kos, sedangkan lama waktu penyewaan ditentukan sendiri oleh penyewa kamar. Untuk mendapatkan informasi kostan biasanya dilakukan dengan datang ke tempat tujuan secara langsung untuk mencari tempat kos-kosan. </p>
-              <p style="color: white;">Kost Tegar merupakan website kosan yang menyediakan banyak kamar yang memiliki fasilitas yang nyaman dan layak. Kost Tegar terletak di Jalan Cengkeh, Gedung Meneng, Rajabasa</p>
+            <span>Tentang Kost Tegar</span>
+            <div style="color: white;">Kos-kosan merupakan salah satu tempat penyedia jasa penginapan atau tempat tinggal sementara yang memiliki beberapa fasilitas yang tersedia, dan juga mempunyai harga yang telah ditentukan oleh pemilik kos, sedangkan lama waktu penyewaan ditentukan sendiri oleh penyewa kamar. Untuk mendapatkan informasi kostan biasanya dilakukan dengan datang ke tempat tujuan secara langsung untuk mencari tempat kos-kosan. </div>
+            <div style="color: white;">Dengan semakin berkembangnya fungsi dari sebuah website, pemilik kos-kos juga dapat mempromosikan tempat kost dengan mudah dan efektif tanpa harus mencetak brosur atau flyer serta dapat mempermudah penyebaran informasi.</div>
+            <div style="color: white;">Kost Tegar merupakan website kosan yang menyediakan banyak kamar yang memiliki fasilitas yang nyaman dan layak. Kost Tegar terletak di Jalan Cengkeh, Gedung Meneng, Rajabasa.</div>
             </div>
             <div class="col-lg-4">
               <div class="main-button">
