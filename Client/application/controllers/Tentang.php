13881087b6688d003445a89844a12cd8f2a11651
Muhan212223 <muhan212223@gmail.com>
2023-01-27 20:54:40 +0700

    abdian: halaman tentang

--- Client/application/controllers/Tentang.php
+++ Client/application/controllers/Tentang.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Tentang extends CI_Controller {
    
     public function index()
 	{
@@ -9,9 +9,7 @@ class Dashboard extends CI_Controller {
 		
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('vw_dashboard');
+        $this->load->view('vw_tentang');
         $this->load->view('templates_admin/footer');
 	}
-
-    
 }
