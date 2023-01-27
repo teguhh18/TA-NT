c380282d31e08056be569afb9b2feb231d50e4ef
Muhan212223 <muhan212223@gmail.com>
2023-01-27 23:24:47 +0700

    abdian: update halaman tentang

--- Client/application/views/vw_tentang.php
+++ Client/application/views/vw_tentang.php
@@ -8,6 +8,7 @@
           <div class="row">
             <div class="col-lg-8">
               <span>Tentang Kost Tegar</span>
+              <p style="color: white;">Kos-kosan merupakan salah satu tempat penyedia jasa penginapan atau tempat tinggal sementara yang memiliki beberapa fasilitas yang tersedia, dan juga mempunyai harga yang telah ditentukan oleh pemilik kos, sedangkan lama waktu penyewaan ditentukan sendiri oleh penyewa kamar. Untuk mendapatkan informasi kostan biasanya dilakukan dengan datang ke tempat tujuan secara langsung untuk mencari tempat kos-kosan. </p>
               <p style="color: white;">Kost Tegar merupakan website kosan yang menyediakan banyak kamar yang memiliki fasilitas yang nyaman dan layak. Kost Tegar terletak di Jalan Cengkeh, Gedung Meneng, Rajabasa</p>
             </div>
             <div class="col-lg-4">
